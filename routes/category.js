f5d25c4c1bc624465cc9086fe8196ee0c1fb26e6
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2023-04-16 11:01:29 +0530

    added cat

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { addCategory, getCategory } from "../controller/category/category";
 
 
 const router = express.Router();
 
 router.post("/addCat", addCategory);
-router.post("/deleteCat", deleteCategory);
+// router.post("/deleteCat", deleteCategory);
 
 
 router.get("/getCat", getCategory);
