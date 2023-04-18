704539b3f69f5b336a44961c9e9d8356fbbd4fb6
akumar <akumar@comtronusa.com>
2023-04-18 02:36:26 -0400

    added

--- model/Products.js
+++ model/Products.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const productSchema = new Schema({
-    product_id: {type: String, unique:true},
-    name: String,
-    price: Number,
-    weight: Number,
-    measurement: String,
-    category: String,
-    image:
-    {
-        type: String,
-        default: 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/lqcm8z8qwhi42efm2lue'
-    },
-    stock: {
-        type: Number,
-        default: 0
-    }
+  product_id: { type: String, unique: true },
+  name: String,
+  price: Number,
+  number: Number,
+  vendor: String,
+  category: String,
+  image: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/lqcm8z8qwhi42efm2lue",
+  },
+  availabe: {
+    type: Boolean,
+    default: true,
+  },
 });
 
-const Products = mongoose.model('Products',productSchema);
+const Products = mongoose.model("Products", productSchema);
 export default Products;
